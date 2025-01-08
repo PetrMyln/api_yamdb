41f128c2e96918506250bb39afff7f936f47d099
Имя ФÐPetrMyln <mylnikovpetr@mail.ru>
2025-01-08 12:31:17 +0300

    add urls serialzr veiws

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from reveiews.models import (
+    MyUser,
+    Categories,
+    Titles,
+    Genre
+)
+
+from api.serializers import (
+    MyUserSerializer,
+    TitlesSerializer,
+    CategoriesSerializer,
+    GenreSerializer
+)
+
+
+class MyUserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = MyUser.objects.all()
+    serializer_class = MyUserSerializer
+
+
+class TitlesViewSet(viewsets.ModelViewSet):
+    queryset = Titles.objects.all()
+    serializer_class = TitlesSerializer
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
