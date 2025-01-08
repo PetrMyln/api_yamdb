76c1fd7cd40f5c823562ebea2d686831c8436d78
Имя ФÐPetrMyln <mylnikovpetr@mail.ru>
2025-01-08 11:02:35 +0300

    test merge develop_petr and develop

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class MyUser(AbstractUser):
+    bio = models.TextField('Биография', blank=True)
+    role = models.TextField('Статус пользователя')
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.DateTimeField(
+        'Дата выхода')
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.CASCADE,
+        related_name='category',
+    )
+    genge = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name='genge',
+    )
