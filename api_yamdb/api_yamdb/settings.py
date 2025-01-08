90529421ce9fd0f49223361e9ed2045c77c1a3c6
Имя ФÐPetrMyln <mylnikovpetr@mail.ru>
2025-01-08 12:44:15 +0300

    test merge 1 vs main test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from datetime import timedelta
 
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'djoser',
+    'users',
     'api',
     'reviews',
 ]
@@ -125,3 +127,4 @@ SIMPLE_JWT = {
 }
 
 AUTH_USER_MODEL = 'users.MyUser'
+
