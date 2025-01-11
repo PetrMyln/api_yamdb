45f718b9e91375314c948ccb2e70abab4b219fd9
Имя ФÐPetrMyln <mylnikovpetr@mail.ru>
2025-01-11 13:33:36 +0300

    bag fix in addcsv command, Start Title modelviewset

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -106,5 +106,4 @@ class TitleSerializersCreateUpdate(serializers.ModelSerializer):
                   # 'rating',
                   'genre',
                   'category',)
-
         model = Title
