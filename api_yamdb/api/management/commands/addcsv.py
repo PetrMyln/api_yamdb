eb9417b769bbf580534f5d80fa3debcdd2888cdf
Имя ФÐPetrMyln <mylnikovpetr@mail.ru>
2025-01-14 23:18:01 +0300

    UPDATE addcsv

--- api_yamdb/api/management/commands/addcsv.py
+++ api_yamdb/api/management/commands/addcsv.py
@@ -1,15 +1,14 @@
-from sqlite3 import connect, IntegrityError
-from csv import DictReader
+import csv
+import sqlite3
 
 from django.core.management.base import BaseCommand
-
-from api_yamdb.constant import STATIC_DIR_FOR_CSV_FILES, FILE_GENRE_TITLE
 from reviews.models import (
     Category,
     Comment,
     Genre,
     Review,
     Title,
+
 )
 from users.models import MyUser
 
@@ -20,7 +19,10 @@ CSV_DATA = {
     Title: 'titles.csv',
     Review: 'review.csv',
     Comment: 'comments.csv',
+
 }
+
+FILE_WITH_TITLES_GENRE = 'static\\data\\genre_title.csv'
 
 
 class Command(BaseCommand):
@@ -28,36 +30,31 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for model, file_name in CSV_DATA.items():
-            name = STATIC_DIR_FOR_CSV_FILES + file_name
-            with open(name, encoding='utf-8') as file:
-                rows = DictReader(file)
+            file_name = 'static\\data' + '\\' + file_name
+            with open(file_name, encoding='utf-8') as file:
+                rows = csv.DictReader(file)
                 for row in rows:
                     try:
                         model.objects.create(**row).save()
                     except Exception:
                         pass
-        con = connect('db.sqlite3')
+        con = sqlite3.connect('db.sqlite3')
         cur = con.cursor()
         data_for_db = list()
-
-        with open(FILE_GENRE_TITLE, encoding='utf-8') as file:
+        with open(FILE_WITH_TITLES_GENRE, encoding='utf-8') as file:
             data = file.readlines()
             for c in data:
                 data_for_db.append(c[:-1].split(','))
-        try:
-            cur.execute('''
-            CREATE TABLE IF NOT EXISTS genre_title (
-            id INTEGER PRIMARY KEY,
-            title_id TEXT  NOT NULL,
-            genre_id TEXT  NOT NULL
-            );
-            ''')
-            con.commit()
-            cur.executemany(
-                'INSERT INTO genre_title VALUES(?, ?, ?)',
-                data_for_db[1:])
-            con.commit()
-        except IntegrityError:
-            pass
-        finally:
-            con.close()
+        cur.execute(''' 
+        CREATE TABLE IF NOT EXISTS genre_title ( 
+        id INTEGER PRIMARY KEY, 
+        title_id TEXT  NOT NULL, 
+        genre_id TEXT  NOT NULL 
+        ); 
+        ''')
+        con.commit()
+        cur.executemany(
+            'INSERT INTO genre_title VALUES(?, ?, ?)',
+            data_for_db[1:])
+        con.commit()
+        con.close()
